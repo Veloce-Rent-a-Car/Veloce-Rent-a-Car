55fb56aa7e3cd94147a1a601c745294fe89c27d6
rossiniroberto52 <toquehuk@gmail.com>
2026-08-13 20:17:21 -0300

    feat: initialize Spring Boot application with Maven wrapper and configuration files

---
+++ v1/src/test/java/Veloce_Rent_a_Car/v1/V1ApplicationTests.java
@@ -0,0 +1,13 @@
+package Veloce_Rent_a_Car.v1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class V1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
