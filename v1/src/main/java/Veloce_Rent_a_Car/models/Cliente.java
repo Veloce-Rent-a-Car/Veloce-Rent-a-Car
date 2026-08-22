ba2eff8362719cf730e4c701947bda7267464716
rossiniroberto52 <toquehuk@gmail.com>
2026-08-22 19:50:36 -0300

    fix geter/seter

--- v1/src/main/java/Veloce_Rent_a_Car/models/Cliente.java
+++ v1/src/main/java/Veloce_Rent_a_Car/models/Cliente.java
@@ -1,5 +1,12 @@
 package Veloce_Rent_a_Car.models;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
 
     private String nome;
@@ -7,44 +14,4 @@ public class Cliente {
     private String telefone;
     private String email;
     private String endereco;
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String telefone) {
-        this.telefone = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getEndereco() {
-        return endereco;
-    }
-
-    public void setEndereco(String endereco) {
-        this.endereco = endereco;
-    }
 }
--- v1/src/main/java/Veloce_Rent_a_Car/models/NewRentModel.java
+++ v1/src/main/java/Veloce_Rent_a_Car/models/NewRentModel.java
@@ -1,4 +1,25 @@
 package Veloce_Rent_a_Car.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NewRentModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private String CarName;
+    private String CarModelType;
+    private float RentValue;
+
 }
