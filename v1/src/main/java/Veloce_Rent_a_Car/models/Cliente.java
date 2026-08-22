187e02ed0ba5466b41617c2eeb53bb3b3a581dca
ronaldo <ronaldomalta9@gmail.com>
2026-08-21 21:53:40 -0300

    feat: adiciona encapsulamento e getters e setters ao cliente

--- v1/src/main/java/Veloce_Rent_a_Car/models/Cliente.java
+++ v1/src/main/java/Veloce_Rent_a_Car/models/Cliente.java
@@ -1,11 +1,50 @@
 package Veloce_Rent_a_Car.models;
 
-
 public class Cliente {
-    public String nome;
-    public String cpf;
-    public String telefone;
-    public String email;
-    public  String endereco;
-    
+
+    private String nome;
+    private String cpf;
+    private String telefone;
+    private String email;
+    private String endereco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
--- v1/src/main/java/Veloce_Rent_a_Car/models/TesteCliente.java
+++ v1/src/main/java/Veloce_Rent_a_Car/models/TesteCliente.java
@@ -1,28 +1,35 @@
-
-    
 package Veloce_Rent_a_Car.models;
 
 public class TesteCliente {
 
     public static void main(String[] args) {
 
-       Cliente cliente = new Cliente();
-        cliente.nome = "Ronaldo";
-        cliente.cpf = "10428298451";
-        cliente.telefone = "81999999999";
-        cliente.email = "cliente@example.com";
-        cliente.endereco = "Recife";
+        Cliente cliente = new Cliente();
 
-        if (cliente.cpf.length() == 11) {
+        cliente.setNome("Ronaldo");
+        cliente.setCpf("10428298451");
+        cliente.setTelefone("81999999999");
+        cliente.setEmail("cliente@example.com");
+        cliente.setEndereco("Recife");
+
+        System.out.println("Nome: " + cliente.getNome());
+
+        if (cliente.getCpf().length() == 11) {
             System.out.println("CPF valido.");
         } else {
             System.out.println("CPF invalido.");
         }
 
-        if (cliente.email.contains("@")) {
+        if (cliente.getEmail().contains("@")) {
             System.out.println("Email valido.");
         } else {
             System.out.println("Email invalido.");
         }
+
+        System.out.println("Nome: " + cliente.getNome());
+        System.out.println("CPF: " + cliente.getCpf());
+        System.out.println("Telefone: " + cliente.getTelefone());
+        System.out.println("Email: " + cliente.getEmail());
+        System.out.println("Endereco: " + cliente.getEndereco());
     }
 }
