740442203effd5705490becd4d53b2a32010850e
Vateshran <pvieira439@gmail.com>
2026-08-25 21:26:42 -0300

    criação do arquivo carro e adição no v1application com base nos dados vindos do arquivo carro

--- v1/src/main/java/Veloce_Rent_a_Car/v1/V1Application.java
+++ v1/src/main/java/Veloce_Rent_a_Car/v1/V1Application.java
@@ -2,34 +2,47 @@ package Veloce_Rent_a_Car.v1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import Veloce_Rent_a_Car.models.Client;
+import Veloce_Rent_a_Car.models.Cliente;
+import Veloce_Rent_a_Car.models.Carro;
 
 @SpringBootApplication
 public class V1Application {
 
     public static void main(String[] args) {
-
         SpringApplication.run(V1Application.class, args);
 
-        Client client = new Client();
+        Cliente cliente = new Cliente();
 
-        client.setName("Ronaldo");
-        client.setCpf("10428298451");
-        client.setPhone("81999999999");
-        client.setEmail("cliente@example.com");
-        client.setAddress("Recife");
+        cliente.nome = "Ronaldo";
+        cliente.cpf = "10428298451";
+        cliente.telefone = "81999999999";
+        cliente.email = "cliente@example.com";
+        cliente.endereco = "Recife";
 
-        if (client.getCpf().length() == 11) {
+        if(cliente.cpf.length() == 11){
             System.out.println("CPF valido.");
-        } else {
+        }else{
             System.out.println("CPF invalido.");
         }
 
-        if (client.getEmail().contains("@")) {
+        if(cliente.email.contains("@")){
             System.out.println("Email valido.");
-        } else {
+        }else{
             System.out.println("Email invalido.");
+        }
+
+        Carro carro = new Carro();
+
+        carro.modelo = "Civic";
+        carro.placa = "PEF4A28";
+        carro.categoria = "sedan";
+        carro.valor_da_diaria = "300";
+        carro.status_de_disponibilidade = "disponivel";
+
+        if (carro.placa.length() == 7) {
+            System.out.println("placa valida");
+        } else {
+            System.out.println("placa invalida");
         }
     }
 }
