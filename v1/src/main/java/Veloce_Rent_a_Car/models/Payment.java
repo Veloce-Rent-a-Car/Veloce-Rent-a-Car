8eceb9264d7cba6a53f3a6da9ba1652ca3b6c5b7
Alexandre <xando.campos@gmail.com>
2026-08-28 12:55:12 -0300

    adicionei repositorio

--- v1/src/main/java/Veloce_Rent_a_Car/models/Payment.java
+++ v1/src/main/java/Veloce_Rent_a_Car/models/Payment.java
@@ -1,11 +1,40 @@
 package Veloce_Rent_a_Car.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "payments")
+
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Aluguel aluguel;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rent_id", nullable = false)
+    private RentModel rent;
+
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal valorPago;
+
+    @Column(nullable = false)
     private LocalDateTime dataPagamento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private MetodoPagamento metodoPagamento;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private StatusPagamento status;
-    
+
 }
